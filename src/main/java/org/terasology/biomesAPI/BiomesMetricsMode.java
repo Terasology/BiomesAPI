e23cdf3accfaf043c999b8d1df76aca065856fe8
vedant-shroff <32770414+vedant-shroff@users.noreply.github.com>
2020-05-14 22:21:10 +0530

    feat: Add Biomes Metrics Mode (#2)
    Co-authored-by: Tobias Nett <skaldarnar@googlemail.com>

---
+++ src/main/java/org/terasology/biomesAPI/BiomesMetricsMode.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2020 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.biomesAPI;
+
+import org.terasology.rendering.nui.layers.ingame.metrics.MetricsMode;
+
+/**
+ * Display the name of the biome the player is currently located in in the debug overlay.
+ * <p>
+ * Current biome is set when OnBiomeChanged event is triggered
+ * biomeName is polled whenever MetricsMode values are updated
+ */
+class BiomesMetricsMode extends MetricsMode {
+
+    private String biomeName;
+
+    BiomesMetricsMode() {
+        super("\n- Biome Info -");
+    }
+
+    @Override
+    public String getMetrics() {
+        return getName()+"\n"+biomeName;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public boolean isPerformanceManagerMode() {
+        return false;
+    }
+
+    public void setBiome(String biomeName) {
+        this.biomeName = biomeName;
+    }
+}
