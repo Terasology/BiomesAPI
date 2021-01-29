c6081d9c12e23121a7eb85a037bfa6dedbf06e23
jdrueckert <jd.rueckert@googlemail.com>
2021-01-29 20:12:15 +0100

    chore(JOML): remove deprecated, no more used functions (#10)

--- src/main/java/org/terasology/biomesAPI/BiomeRegistry.java
+++ src/main/java/org/terasology/biomesAPI/BiomeRegistry.java
@@ -1,23 +1,9 @@
-/*
- * Copyright 2018 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.biomesAPI;
 
 import org.joml.Vector3ic;
 import org.terasology.entitySystem.systems.ComponentSystem;
-import org.terasology.math.geom.Vector3i;
 import org.terasology.world.chunks.CoreChunk;
 
 import java.util.Collection;
@@ -54,11 +40,6 @@ public interface BiomeRegistry {
         setBiome(biome, pos.x(), pos.y(), pos.z());
     }
 
-    @Deprecated
-    default void setBiome(Biome biome, Vector3i pos) {
-        setBiome(biome, pos.x, pos.y, pos.z);
-    }
-
     /**
      * Sets specified biome at position in chunk.
      *
@@ -81,11 +62,6 @@ public interface BiomeRegistry {
         setBiome(biome, chunk, pos.x(), pos.y(), pos.z());
     }
 
-    @Deprecated
-    default void setBiome(Biome biome, CoreChunk chunk, Vector3i pos) {
-        setBiome(biome, chunk, pos.x, pos.y, pos.z);
-    }
-
     /**
      * Gets biome at position in world.
      *
@@ -95,11 +71,6 @@ public interface BiomeRegistry {
     default Optional<Biome> getBiome(Vector3ic pos) {
         return getBiome(pos.x(), pos.y(), pos.z());
     };
-
-    @Deprecated
-    default Optional<Biome> getBiome(Vector3i pos) {
-        return getBiome(pos.x, pos.y, pos.z);
-    }
 
     /**
      * Gets biome at position in world.
