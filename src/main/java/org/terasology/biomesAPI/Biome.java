5d2c7423826934a3168b83fa855cb888870c2f02
soloturn <soloturn@gmail.com>
2023-12-27 19:09:12 +0100

    biome qa, checkstyle, spotbugs
    * use standard comments
    * shorten line lenght
    * logger was defined twice
    * use {} for logger parameters
    * whitespaces so no warning is produced

--- src/main/java/org/terasology/biomesAPI/Biome.java
+++ src/main/java/org/terasology/biomesAPI/Biome.java
@@ -1,18 +1,6 @@
-/*
- * Copyright 2018 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2018 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
+
 package org.terasology.biomesAPI;
 
 import org.joml.Vector3ic;
@@ -22,8 +10,8 @@ import org.terasology.engine.world.block.BlockManager;
 import org.terasology.gestalt.naming.Name;
 
 /**
- * Biomes can be assigned to different blocks during worldgen as well as on runtime, to provide additional metadata
- * about player's surroundings usable to enhance player experience.
+ * Biomes can be assigned to different blocks during worldgen as well as on runtime, to provide additional metadata about player's
+ * surroundings usable to enhance player experience.
  * <p>
  * Biomes are easiest implemented in enums, and are meant to be implemented like that.
  *
@@ -32,8 +20,7 @@ import org.terasology.gestalt.naming.Name;
 public interface Biome {
 
     /**
-     * @return An identifier that includes both the Module the biome originates from
-     * and a unique biome id (unique to that module).
+     * @return An identifier that includes both the Module the biome originates from and a unique biome id (unique to that module).
      */
     Name getId();
 
@@ -45,7 +32,8 @@ public interface Biome {
     /**
      * Runs any necessary initialization, like caching block types, when the biome is registered.
      */
-    default void initialize() {}
+    default void initialize() {
+    }
 
     /**
      * @return An average humidity value for the biome, used for grass and foliage colors.
@@ -62,8 +50,7 @@ public interface Biome {
     }
 
     /**
-     * @return The block that should be generated as the top layer of the biome at the given position.
-     * Defaults to grass.
+     * @return The block that should be generated as the top layer of the biome at the given position. Defaults to grass.
      */
     default Block getSurfaceBlock(Vector3ic pos, int seaLevel) {
         BlockManager blockManager = CoreRegistry.get(BlockManager.class);
@@ -71,8 +58,8 @@ public interface Biome {
     }
 
     /**
-     * @return The block that should be generated at the given position in the biome, which isn't the surface.
-     * Defaults to dirt for the first 32 blocks and then stone below.
+     * @return The block that should be generated at the given position in the biome, which isn't the surface. Defaults to dirt for the
+     *         first 32 blocks and then stone below.
      */
     default Block getBelowSurfaceBlock(Vector3ic pos, float density) {
         BlockManager blockManager = CoreRegistry.get(BlockManager.class);
@@ -87,9 +74,9 @@ public interface Biome {
     /**
      * Biome hashCode must be deterministic, non-zero, and unique for every biome.
      * <p>
-     * Please consider overriding this method to return constant values, hard-coded for each of the biomes.
-     * No assumptions should however be made from any external module using biomes about their constant value,
-     * i.e. modules should always retrieve biome hash using this function, and not hard-code any constant values.
+     * Please consider overriding this method to return constant values, hard-coded for each of the biomes. No assumptions should however be
+     * made from any external module using biomes about their constant value, i.e. modules should always retrieve biome hash using this
+     * function, and not hard-code any constant values.
      *
      * @return Hashcode of the biome
      */
